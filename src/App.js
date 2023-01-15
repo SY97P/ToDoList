f4562fddda40facd23b5e9f4cf675a6ddefa8e02
SY97P <onetuks@naver.com>
2023-01-15 06:24:53 +0000

    To Do List done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+	const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+	
+	const onChange = (event) => {
+		setTodo(event.target.value);
+		// console.log(event.target.value);
+	};
+	
+	
+	
+	function onSubmit(event) {
+		// preventDefault()
+		// 1. a 태그를 눌러도 href 링크로 이동하지 않도록 하는 용도
+		// 2. form 버튼을 눌러 submit을 해도 페이지를 새로고침하고 싶지 않을 때
+		// 이거 없으면 form 버튼 눌러서 submit 할 때 내가 만든 input text가 날라감
+		event.preventDefault();
+		
+		if (todo === "") {
+			return ;
+		}
+		setTodos((current) => [todo, ...current]);
+		setTodo("");
+	}
+	
+	return (
+		<div className="App">
+			<div style={{backgroundColor:"black", color:"white"}}>
+				<img src={logo} alt="logo" height="50px" width="50px"/>
+				<h4><b>To do List</b></h4>
+			</div>
+			<h1>My Todos ({todos.length})</h1>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={todo}
+					type="text"
+					placeholder="Write your to do"
+				/>
+				<button>Add To Do</button>
+			</form>
+			<hr />
+			{todos.map((item, index) => (
+				<li key={index}>{index} : {item}</li>
+			))}
+		</div>
+	);
 }
-
 export default App;
